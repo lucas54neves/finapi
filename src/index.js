11f770d95f0a8183435eedbd70fcff126347d8a9
Lucas Neves <lucas54neves@outlook.com>
2021-04-07 19:53:09 -0300

    add validation for existing cpf

--- src/index.js
+++ src/index.js
@@ -14,6 +14,14 @@ app.get("/", (request, response) => {
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body
 
+  const customerAlreadyExists = customers.some(
+    customer => customer.cpf === cpf
+  )
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: "Customer already exists!" })
+  }
+
   const id = uuidv4()
 
   customers.push({
