d5a7c5804a15196c7da951ff71e0ebc6c3f3a3dc
Lucas Neves <lucas54neves@outlook.com>
2021-04-07 19:48:00 -0300

    add account creation

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 const express = require('express')
+const { v4: uuidv4 } = require('uuid')
 
 const app = express()
+
+const customers = []
 
 app.use(express.json())
 
 app.get("/", (request, response) => {
-  return response.json({ message: "Hello world Ingnite!" })
+  return response.json({ message: "FinAPI" })
+})
+
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body
+
+  const id = uuidv4()
+
+  customers.push({
+    cpf,
+    name,
+    id,
+    statement: []
+  })
+
+  return response.status(201).send()
 })
 
 app.listen(3333, () => {
