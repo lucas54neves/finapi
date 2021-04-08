1ae53bf41e5452df4589985effc9629349650e99
Lucas Neves <lucas54neves@outlook.com>
2021-04-07 22:04:22 -0300

    add statement listing

--- src/index.js
+++ src/index.js
@@ -34,6 +34,18 @@ app.post("/account", (request, response) => {
   return response.status(201).send()
 })
 
+app.get("/statement/:cpf", (request, response) => {
+  const { cpf } = request.params
+
+  const customer = customers.find(customer => customer.cpf === cpf)
+
+  if (!customer) {
+    return response.status(400).json({ error: { message:"Customer not found" }})
+  }
+  
+  return response.json(customer.statement)
+})
+
 app.listen(3333, () => {
   console.log('Server started on port 3333')
 })
